7f46f9cbfb37b86a1830cc01de63a9db47c3e3de
Hector Tenezaca <h.tenezaca@uniandes.edu.co>
2022-10-29 20:11:52 -0500

    feat: email provider using API (SendGrid)

--- services/converter/email_service.py
+++ services/converter/email_service.py
@@ -1,42 +1,35 @@
-import smtplib
 import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
+BASE_URL = os.environ.get("BASE_URL")
+API_KEY = os.environ.get("SENDGRID_API_KEY")
+EMAIL_CONVERTER = os.environ.get("EMAIL_CONVERTER")
 EMAIL_USER = os.environ.get("EMAIL_USER")
-EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
-STRESS_TEST = os.environ.get("STRESS_TEST", False)
-BASE_URL = os.environ.get("BASE_URL", "").rstrip("/")
+STRESS_TEST = os.environ.get("STRESS_TEST", True)
 
-if STRESS_TEST == '0':
+if STRESS_TEST == "0":
     STRESS_TEST = False
-
-if not EMAIL_USER or not EMAIL_PASSWORD or not BASE_URL:
-    raise ValueError(
-        "Missing EMAIL_USER, EMAIL_PASSWORD or BASE_URL environment variable"
-    )
-
-
-s = smtplib.SMTP("smtp.gmail.com", 587)
-s.starttls()
-s.login(EMAIL_USER, EMAIL_PASSWORD)
 
 
 def send_notification(email, file_id):
     from app import logger
-
     if STRESS_TEST:
         return
 
-    url = f"{BASE_URL}/api/files/{file_id}"
-    logger.info(f"Sending email to {email} with link {url}")
-    s.sendmail(
-        EMAIL_USER,
-        "graulopezjavier@gmail.com",
-        """
-        Subject: Archivo convertido
-
-        La conversion de su archivo ha finalizado. Ingrese a la plataforma para descargarlo.
-        {}
-        """.format(
-            url
+    message = Mail(
+        from_email=EMAIL_CONVERTER,
+        to_emails=EMAIL_USER,
+        subject="Archivo convertido",
+        html_content='<p>Descargar archivo: <a href="{0}api/files/{1}">aqui</a></p>'.format(
+            BASE_URL, file_id
         ),
     )
+    try:
+        sg = SendGridAPIClient(API_KEY)
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        logger.error(f"Error sending email: {e}")
